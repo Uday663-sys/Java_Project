b11ccb693bf14e71d9cc54562622d6be2dd371ea
Uday663-sys <udayp9375@gmail.com>
2026-08-17 16:58:45 +0530

    Add files via upload

---
+++ areaoftriangle.java
@@ -0,0 +1,10 @@
+package IOT;
+
+public class areaoftriangle {
+	public static void main(String[] args) {
+		double base=3;
+		double height=4;
+		double area=0.5*base*height;
+		System.out.println(area);
+	}
+}
---
+++ fibonaci.java
@@ -0,0 +1,14 @@
+package IOT;
+
+public class fibonaci {
+	public static void main(String[] args) {
+		int n=5;
+		int a=0,b=1;
+		for(int i=1; i<=n; i++) {
+			System.out.println(a+"");
+			int c=a+b;
+			a=b;
+			b=c;
+		}
+	}
+}
